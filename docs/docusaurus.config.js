27107ada158d341db056da753c086c36582f76b8
Anton <bayselonarrend@gmail.com>
2024-01-14 01:48:49 +0300

    Документация Docusaurus

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,107 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Открытый Пакет Интеграций',
+  tagline: 'Набор библиотек для интеграции с популярными API на 1C:Enterprise',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Bayselonarrend', // Usually your GitHub org/user name.
+  projectName: 'OpenIntegrations', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'ru',
+    locales: ['ru'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+
+      navbar: {
+        title: 'Открытый пакет интеграций',
+        logo: {
+          alt: 'OPI',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'defaultSidebar',
+            position: 'left',
+            label: 'Описания методов',
+          },
+          {to: '/blog', label: 'Список изменений', position: 'left'},
+          {
+            href: 'https://github.com/Bayselonarrend/OpenIntegrations',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Документация',
+            items: [
+              {
+                label: 'Описания методов',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Ресурсы',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/Bayselonarrend/OpenIntegrations',
+              },
+            ],
+          },
+        ],
+        copyright: `${new Date().getFullYear()} OPI by Bayselonarrend`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+		additionalLanguages: ['json','bsl']
+      },
+    }),
+};
+
+export default config;
