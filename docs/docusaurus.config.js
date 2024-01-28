078ff04ea91dc414117b75e1e6a2f6396ecd275b
Anton <bayselonarrend@gmail.com>
2024-01-29 00:55:00 +0300

    Заменил имена файлов документации на транслит латиницей

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://opi.neocities.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -80,7 +80,7 @@ const config = {
             items: [
               {
                 label: 'Описания методов',
-                to: '/docs/Начало%20работы',
+                to: '/docs/Nachalo-raboty',
               },
             ],
           },
--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/Начало работы">
+            to="/docs/Nachalo-raboty">
             Начать
           </Link>
         </div>
