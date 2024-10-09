c3dd4bbc1695e6aa1509b88532b49bfdb1007d34
Anton <bayselonarrend@gmail.com>
2024-10-09 20:02:55 +0300

    Правки доков

---
+++ docs/docusaurus/src/theme/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Footer from '@theme-original/Footer';
+
+export default function FooterWrapper(props) {
+  return (
+    <>
+      <div style={{height:150 + 'px'}} id="yandex_rtb_R-A-12294791-1"></div>
+
+      <Footer {...props} />
+    </>
+  );
+}
