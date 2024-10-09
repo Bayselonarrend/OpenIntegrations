2fe5a02e05064196d1584ee333875fdc2bac9cd7
Anton <bayselonarrend@gmail.com>
2024-10-09 20:52:28 +0300

    DCS Layout

---
+++ docs/docusaurus/src/theme/Layout.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import Layout from '@theme-original/Layout';
+import Head from '@docusaurus/Head';
+
+export default function CustomLayout(props) {
+  useEffect(() => {
+    // Создаем <div> для блока Яндекса в конце body
+    const adDiv = document.createElement('div');
+    adDiv.id = 'yandex_rtb_R-A-12294791-1';
+    document.body.appendChild(adDiv);
+
+    // Добавляем скрипт для отображения рекламы
+    const script = document.createElement('script');
+    script.innerHTML = `
+      window.yaContextCb.push(() => {
+        Ya.Context.AdvManager.render({
+          blockId: "R-A-12294791-1",
+          renderTo: "yandex_rtb_R-A-12294791-1",
+          type: "feed"
+        });
+      });
+    `;
+    document.body.appendChild(script);
+
+    // Удаляем элементы при размонтировании компонента
+    return () => {
+      document.body.removeChild(adDiv);
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <script>
+          window.yaContextCb = window.yaContextCb || [];
+        </script>
+        <script src="https://yandex.ru/ads/system/context.js" async></script>
+      </Head>
+      <Layout {...props} />
+    </>
+  );
+}
