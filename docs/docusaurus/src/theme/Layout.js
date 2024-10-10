ad60155200f6bf5e8d183e11198c99f0ed36c2c8
Anton Titovets <bayselonarrend@gmail.com>
2024-10-10 09:15:58 +0300

    Update Layout.js

--- docs/docusaurus/src/theme/Layout.js
+++ docs/docusaurus/src/theme/Layout.js
@@ -1,37 +1,55 @@
 import React, { useEffect } from 'react';
 import Layout from '@theme-original/Layout';
 import Head from '@docusaurus/Head';
+import { useLocation } from '@docusaurus/router';
 
 export default function CustomLayout(props) {
-  useEffect(() => {
+  const location = useLocation();
 
+  useEffect(() => {
+    // Проверяем, находимся ли мы на странице документации
     if (!location.pathname.startsWith('/docs/')) {
-      return; // Если нет, не добавляем блок
+      return; // Если нет, не выполняем код для рекламы
     }
 
-    const script = document.createElement('script');
-    script.innerHTML = `
-window.yaContextCb.push(() => {
-    Ya.Context.AdvManager.render({
-        "blockId": "R-A-12294791-3",
-        "type": "floorAd",
-        "platform": "touch"
-    });
-    Ya.Context.AdvManager.render({
-        "blockId": "R-A-12294791-4",
-        "type": "floorAd",
-        "platform": "desktop"
-    });
+    // Функция для рендеринга рекламы
+    const renderAds = () => {
+      // Удаляем предыдущий скрипт рендеринга, если он существует
+      const existingScript = document.getElementById('yandex-ad-script');
+      if (existingScript) {
+        existingScript.remove();
+      }
 
-})
-    `;
-    document.body.appendChild(script);
+      // Создаем новый скрипт для рендеринга рекламы
+      const script = document.createElement('script');
+      script.id = 'yandex-ad-script';
+      script.innerHTML = `
+      window.yaContextCb.push(() => {
+          Ya.Context.AdvManager.render({
+              "blockId": "R-A-12294791-3",
+              "type": "floorAd",
+              "platform": "touch"
+          });
+          Ya.Context.AdvManager.render({
+              "blockId": "R-A-12294791-4",
+              "type": "floorAd",
+              "platform": "desktop"
+          });
 
-    // Удаляем элементы при размонтировании компонента
-    return () => {
-      script.remove();
+      })
+          `;
+      document.body.appendChild(script);
     };
-  }, []);
+
+    // Рендерим рекламные блоки при загрузке страницы или изменении маршрута
+    renderAds();
+
+    // Удаляем блоки при размонтировании или смене страницы
+    return () => {
+      const existingScript = document.getElementById('yandex-ad-script');
+      if (existingScript) existingScript.remove();
+    };
+  }, [location.pathname]); // Следим за изменением пути
 
   return (
     <>
