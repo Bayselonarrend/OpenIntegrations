41bc6885dd30dae5959824d6ae79f300eb259a93
Anton Titovets <bayselonarrend@gmail.com>
2024-12-13 08:50:48 +0300

    Обновлен шаблон компоненты

--- src/addins/tmpl/release.bat
+++ src/addins/tmpl/release.bat
@@ -1,2 +1,63 @@
+@echo off
+
+:: Установить переменную
+set LIB_NAME=opi_addin
+set OPENSSL_DIR=C:\msys64\mingw64
+set OPENSSL_LIB_DIR=%OPENSSL_DIR%\lib
+set OPENSSL_INCLUDE_DIR=%OPENSSL_DIR%\include
+
+
+:: Перейти в директорию проекта
+cd /d "%~dp0"
+
+:: Создать папку для артефактов
+set OUTPUT_DIR=artifacts
+if not exist "%OUTPUT_DIR%" mkdir "%OUTPUT_DIR%"
+
+:: Сборка для x86_64-pc-windows-msvc
 cargo build --release --target x86_64-pc-windows-msvc
-cargo zigbuild --release --target x86_64-unknown-linux-gnu 
+if errorlevel 1 goto :error
+
+:: Сборка для x86_64-unknown-linux-gnu
+cargo zigbuild --release --target x86_64-unknown-linux-gnu
+if errorlevel 1 goto :error
+
+:: Сборка для i686-pc-windows-msvc
+cargo build --release --target i686-pc-windows-msvc
+if errorlevel 1 goto :error
+
+:: Сборка для i686-unknown-linux-gnu
+cargo zigbuild --release --target i686-unknown-linux-gnu
+if errorlevel 1 goto :error
+
+:: Копирование файлов .dll и .so
+copy /y target\x86_64-pc-windows-msvc\release\%LIB_NAME%.dll "%OUTPUT_DIR%\AddIn_x64_windows.dll"
+if errorlevel 1 goto :error
+
+copy /y target\i686-pc-windows-msvc\release\%LIB_NAME%.dll "%OUTPUT_DIR%\AddIn_x86_windows.dll"
+if errorlevel 1 goto :error
+
+copy /y target\x86_64-unknown-linux-gnu\release\lib%LIB_NAME%.so "%OUTPUT_DIR%\AddIn_x64_linux.so"
+if errorlevel 1 goto :error
+
+copy /y target\i686-unknown-linux-gnu\release\lib%LIB_NAME%.so "%OUTPUT_DIR%\AddIn_x86_linux.so"
+if errorlevel 1 goto :error
+
+copy /y MANIFEST.XML "%OUTPUT_DIR%\MANIFEST.XML"
+if errorlevel 1 goto :error
+
+:: Архивация
+set ZIP_NAME=addin.zip
+powershell -Command "Compress-Archive -Path '%OUTPUT_DIR%\*' -Force -DestinationPath '%ZIP_NAME%'"
+if errorlevel 1 goto :error
+
+if exist "%OUTPUT_DIR%" (
+    rmdir /S /Q "%OUTPUT_DIR%"
+)
+
+@echo Build and packaging completed successfully.
+exit /b 0
+
+:error
+@echo An error occurred during the build or packaging process.
+exit /b 1
--- src/addins/tmpl/src/component/methods.rs
+++ src/addins/tmpl/src/component/methods.rs
@@ -1,18 +1,4 @@
-use addin1c::{Variant};
-use crate::component::AddIn;
-
-pub fn send_message(obj: &AddIn, params: &[Variant]) -> String {
-
-    let field1 = &obj.field1;
-    params[0].get_string().unwrap_or("".to_string()) + field1
-
+pub fn method1() -> bool {
+    true
 }
 
-pub fn amount(obj: &AddIn, params: &mut [Variant]) -> i32 {
-
-    let result = params[0].get_i32().unwrap() + params[1].get_i32().unwrap();
-    params[0].set_i32(999);
-
-    result
-
-}
