06ae318609f59d1b9c57fdba9b34f4ae7742f249
Anton Titovets <bayselonarrend@gmail.com>
2024-12-14 17:30:41 +0300

    Доработки по TCP

--- src/addins/tcp/src/component/methods.rs
+++ src/addins/tcp/src/component/methods.rs
@@ -1,69 +1,36 @@
 use std::io::{Read, Write};
 use std::net::TcpStream;
-use native_tls::TlsStream;
-use std::time::{Duration, Instant};
-
-pub enum Connection {
-    Tcp(TcpStream),
-    Tls(TlsStream<TcpStream>),
-}
-
-impl Connection {
-    pub fn write(&mut self, data: &[u8]) -> std::io::Result<usize> {
-        match self {
-            Connection::Tcp(stream) => stream.write(data),
-            Connection::Tls(stream) => stream.write(data),
-        }
-    }
-
-    pub fn read(&mut self, buffer: &mut [u8]) -> std::io::Result<usize> {
-        match self {
-            Connection::Tcp(stream) => stream.read(buffer),
-            Connection::Tls(stream) => stream.read(buffer),
-        }
-    }
-}
+use std::time::{Duration};
 
 /// Отправляет данные
-pub fn send(connection: &mut Connection, data: Vec<u8>, timeout_ms: i32) -> bool {
+pub fn send(connection: &mut TcpStream, data: Vec<u8>, timeout_ms: i32) -> bool {
 
-    match connection {
-        Connection::Tcp(stream) => {
-            stream.set_write_timeout(Some(Duration::from_millis(timeout_ms as u64))).ok();
-        }
-        Connection::Tls(stream) => {
-            stream.get_ref().set_write_timeout(Some(Duration::from_millis(timeout_ms as u64))).ok();
-        }
+    if timeout_ms > 0 {
+        let timeout = Duration::from_millis(timeout_ms as u64);
+        connection.set_write_timeout(Some(timeout)).ok();
     }
 
     match connection.write(&data) {
-        Ok(_) => true,
+        Ok(_) => connection.flush().is_ok(),
         Err(_) => false, // Ошибка при отправке данных
     }
 }
 
 /// Считывает данные
 pub fn receive(
-    connection: &mut Connection,
-    timeout_ms: i32,
-    max_attempts: i32,
+    connection: &mut TcpStream,
     max_data_size: i32,
+    timeout_ms: i32,
 ) -> Vec<u8> {
-    const BUFFER_SIZE: usize = 4096; // Фиксированный размер буфера
+    const BUFFER_SIZE: usize = 1024; // Фиксированный размер буфера
 
     let mut result = Vec::new();
     let mut buffer = vec![0u8; BUFFER_SIZE];
-    let mut attempts = 0;
 
     // Устанавливаем таймаут для чтения
-    let timeout = Duration::from_millis(timeout_ms as u64);
-    match connection {
-        Connection::Tcp(stream) => {
-            stream.set_read_timeout(Some(timeout)).ok();
-        }
-        Connection::Tls(stream) => {
-            stream.get_ref().set_read_timeout(Some(timeout)).ok();
-        }
+    if timeout_ms > 0 {
+        let timeout = Duration::from_millis(timeout_ms as u64);
+        connection.set_read_timeout(Some(timeout)).ok();
     }
 
     loop {
@@ -74,18 +41,9 @@ pub fn receive(
 
         match connection.read(&mut buffer) {
             Ok(0) => break, // Конец данных (EOF)
-            Ok(size) => {
-                result.extend_from_slice(&buffer[..size]);
-                attempts = 0; // Сбрасываем счетчик попыток при успешном чтении
-            }
-            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-                attempts += 1;
-                if attempts >= max_attempts {
-                    break; // Превышено число попыток ожидания
-                }
-                continue; // Пробуем снова
-            }
-            Err(_) => break // Любая другая ошибка
+            Ok(size) => result.extend_from_slice(&buffer[..size]),
+            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => break,
+            Err(_) => break
         }
     }
 
