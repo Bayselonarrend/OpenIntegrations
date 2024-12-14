b2ca65863b4ee8692f319f15a171ec69df206b66
Anton Titovets <bayselonarrend@gmail.com>
2024-12-14 17:30:41 +0300

    Доработки по TCP

--- src/addins/tcp/src/component/mod.rs
+++ src/addins/tcp/src/component/mod.rs
@@ -2,9 +2,7 @@ mod methods;
 
 use addin1c::{name, Variant};
 use crate::core::getset;
-use methods::Connection;
 use std::net::TcpStream;
-use native_tls::{TlsConnector};
 use crate::component::methods::disconnect;
 
 // МЕТОДЫ КОМПОНЕНТЫ -------------------------------------------------------------------------------
@@ -14,7 +12,7 @@ pub const METHODS: &[&[u16]] = &[
     name!("Connect"),    // 0
     name!("Disconnect"), // 1
     name!("Read"),       // 2
-    name!("Write"),      // 3
+    name!("Send"),      // 3
 ];
 
 // Число параметров функций компоненты
@@ -22,7 +20,7 @@ pub fn get_params_amount(num: usize) -> usize {
     match num {
         0 => 0,
         1 => 0,
-        2 => 3,
+        2 => 2,
         3 => 2,
         _ => 0,
     }
@@ -43,11 +41,10 @@ pub fn cal_func(obj: &mut AddIn, num: usize, params: &mut [Variant]) -> Box<dyn 
 
 
             let timeout     = params[0].get_i32().unwrap_or(0);
-            let max_attempts= params[1].get_i32().unwrap_or(0);
             let maxsize     = params[2].get_i32().unwrap_or(0);
 
             if let Some(ref mut connection) = obj.connection {
-                Box::new(methods::receive(connection, timeout, max_attempts, maxsize))
+                Box::new(methods::receive(connection, maxsize, timeout))
             } else {
                 Box::new(Vec::<u8>::new()) // Если соединения нет, возвращаем пустой массив
             }
@@ -56,7 +53,7 @@ pub fn cal_func(obj: &mut AddIn, num: usize, params: &mut [Variant]) -> Box<dyn 
 
             let empty_array: [u8; 0] = [];
 
-            let data   = params[0].get_blob().unwrap_or(&empty_array);
+            let data = params[0].get_blob().unwrap_or(&empty_array);
             let timeout = params[1].get_i32().unwrap_or(0);
 
             if let Some(ref mut connection) = obj.connection {
@@ -76,15 +73,13 @@ pub fn cal_func(obj: &mut AddIn, num: usize, params: &mut [Variant]) -> Box<dyn 
 
 // Синонимы
 pub const PROPS: &[&[u16]] = &[
-    name!("Address"),
-    name!("SSL"),
+    name!("Address")
 ];
 
 
 pub struct AddIn {
     pub address: String,
-    pub use_ssl: bool,
-    connection: Option<Connection>,
+    connection: Option<TcpStream>,
 }
 
 impl AddIn {
@@ -92,48 +87,29 @@ impl AddIn {
     pub fn new() -> Self {
         AddIn {
             address: String::new(),
-            use_ssl: false,
             connection: None,
         }
     }
 
     /// Подключается к серверу
     pub fn connect(&mut self) -> bool {
+
         if self.address.is_empty() {
             return false; // Ошибка: пустой адрес
         }
 
-        if self.use_ssl {
-            let connector = match TlsConnector::new() {
-                Ok(conn) => conn,
-                Err(_) => return false, // Ошибка при создании TLS-коннектора
-            };
-            let stream = match TcpStream::connect(&self.address) {
-                Ok(s) => s,
-                Err(_) => return false, // Ошибка при подключении
-            };
-            match connector.connect(&self.address, stream) {
-                Ok(tls_stream) => {
-                    self.connection = Some(Connection::Tls(tls_stream));
-                    true
-                }
-                Err(_) => false, // Ошибка при установлении TLS
+        match TcpStream::connect(&self.address) {
+            Ok(tcp_stream) => {
+                self.connection = Some(tcp_stream);
+                true
             }
-        } else {
-            match TcpStream::connect(&self.address) {
-                Ok(tcp_stream) => {
-                    self.connection = Some(Connection::Tcp(tcp_stream));
-                    true
-                }
-                Err(_) => false, // Ошибка при подключении
-            }
+            Err(_) => false, // Ошибка при подключении
         }
     }
 
     pub fn get_field_ptr(&self, index: usize) -> *const dyn getset::ValueType {
         match index {
             0 => &self.address as &dyn getset::ValueType as *const _,
-            1 => &self.use_ssl as &dyn getset::ValueType as *const _,
             _ => panic!("Index out of bounds"),
         }
     }
