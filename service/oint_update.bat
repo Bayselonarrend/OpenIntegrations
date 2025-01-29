beae20636a76412189d1f9cc84c6cd69aaa48c66
Anton Titovets <bayselonarrend@gmail.com>
2025-01-29 19:39:26 +0300

    Fastfix

--- service/oint_update.bat
+++ service/oint_update.bat
@@ -1,1 +1,1 @@
-cd ..\src\ru\OInt && opm build && opm install -f oint-1.18.0.ospx && del oint-1.18.0.ospx && cd ..\cli && opm build && opm install -f oint-cli-1.18.0.ospx && del oint-cli-1.18.0.ospx
+cd ..\src\ru\OInt && opm build && opm install -f oint-1.18.0.ospx && del oint-1.18.0.ospx && cd ..\cli && opm build && opm install -f oint-cli-1.18.0.ospx && del oint-cli-1.18.0.ospx && cd ..\..\..\service
