bedcde478ec93c23d29b081ca3a53f3900af728d
Anton Titovets <bayselonarrend@gmail.com>
2025-02-03 20:07:20 +0300

    Create oint_update_en.bat

---
+++ service/oint_update_en.bat
@@ -0,0 +1,1 @@
+cd ..\src\en\OInt && opm build && opm install -f oint-1.18.0.ospx && del oint-1.18.0.ospx && cd ..\cli && opm build && opm install -f oint-cli-1.18.0.ospx && del oint-cli-1.18.0.ospx && cd ..\..\..\service
