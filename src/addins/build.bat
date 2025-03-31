ccdd68879c87c1b8309813c69a6fe95a9112b501
Anton Titovets <bayselonarrend@gmail.com>
2025-03-31 14:32:28 +0300

    Пересборка через wsl + zigbuild под glibc 2.18

--- src/addins/build.bat
+++ src/addins/build.bat
@@ -6,16 +6,11 @@ set "THIS_DIR=%THIS_DIR:\=/%"
 :: Перейти в директорию проекта
 cd /d %ADDIN_DIR%
 
-goto :next
-
 :: Создать папку для артефактов
 set OUTPUT_DIR=artifacts
 if not exist "%OUTPUT_DIR%" mkdir "%OUTPUT_DIR%"
 
 :: СБОРКА ОСНОВНОЙ ВЕРСИИ ----------------------------------------------------------------------------------------------
-
-cross clean
-cargo clean
 
 set CROSS_CONFIG=%THIS_DIR%CROSS_MAIN.toml
 
@@ -24,7 +19,7 @@ cargo build --release --target x86_64-pc-windows-msvc
 if errorlevel 1 goto :error
 
 :: Сборка для x86_64-unknown-linux-gnu
-cross build --release --target x86_64-unknown-linux-gnu
+wsl -d OracleLinux_9_1 env LIBRARY_PATH=/usr/lib64 OPENSSL_DIR=/usr cargo zigbuild --release --target x86_64-unknown-linux-gnu
 if errorlevel 1 goto :error
 
 :: Сборка для i686-pc-windows-msvc
@@ -32,7 +27,7 @@ cargo build --release --target i686-pc-windows-msvc
 if errorlevel 1 goto :error
 
 :: Сборка для i686-unknown-linux-gnu
-cross build --release --target i686-unknown-linux-gnu
+wsl -d OracleLinux_9_1 env LIBRARY_PATH=/usr/lib OPENSSL_DIR=/usr OPENSSL_LIB_DIR=/usr/lib/ OPENSSL_INCLUDE_DIR=/usr/include cargo zigbuild --release --target i686-unknown-linux-gnu
 if errorlevel 1 goto :error
 
 :: Копирование файлов .dll и .so
@@ -51,6 +46,10 @@ if errorlevel 1 goto :error
 copy /y ..\MANIFEST_MAIN.XML "%OUTPUT_DIR%\MANIFEST.XML"
 if errorlevel 1 goto :error
 
+echo "MAIN ---" > dependencies.log
+wsl -d OracleLinux_8_7 ldd %OUTPUT_DIR%/AddIn_x64_linux.so >> dependencies.log
+wsl -d OracleLinux_8_7 strings %OUTPUT_DIR%/AddIn_x64_linux.so | grep GLIBC_ >> dependencies.log
+
 :: Архивация
 powershell -Command "Compress-Archive -Path '%OUTPUT_DIR%\*' -Force -DestinationPath '%LIB_NAME%.zip'"
 if errorlevel 1 goto :error
@@ -65,8 +64,15 @@ if exist "%OUTPUT_DIR%" (
     rmdir /S /Q "%OUTPUT_DIR%"
 )
 
+del "%LIB_NAME%.zip"
+
 @echo (MAIN) Build and packaging completed successfully.
-goto :next
+
+IF "%FOLV%"=="true" (
+    goto :next
+) else (
+    exit /b 0
+)
 
 :error
 @echo (MAIN) An error occurred during the build or packaging process.
@@ -74,9 +80,6 @@ exit /b 1
 
 :next
 :: СБОРКА ВЕРСИИ ДЛЯ СТАРЫХ СИСТЕМ -------------------------------------------------------------------------------------
-
-cross clean
-cargo clean
 
 :: Создать папку для артефактов
 set OUTPUT_DIR=artifacts_folv
@@ -86,11 +89,11 @@ set LIB_NAME=%LIB_NAME%_FOLV
 set CROSS_CONFIG=%THIS_DIR%CROSS_FOLV.toml
 
 :: Сборка для x86_64-unknown-linux-gnu
-cross build --release --target x86_64-unknown-linux-gnu
+wsl -d OracleLinux_9_1 env LIBRARY_PATH=/usr/lib64/openssl11/ OPENSSL_DIR=/usr OPENSSL_LIB_DIR=/usr/lib64/openssl11/ cargo zigbuild --release --target x86_64-unknown-linux-gnu
 if errorlevel 1 goto :error
 
 :: Сборка для i686-unknown-linux-gnu
-cross build --release --target i686-unknown-linux-gnu
+wsl -d OracleLinux_9_1 env LIBRARY_PATH=/opt/openssl11-i386/lib OPENSSL_DIR=/opt/openssl11-i386 OPENSSL_LIB_DIR=/opt/openssl11-i386/lib/ OPENSSL_INCLUDE_DIR=/opt/openssl11-i386/include cargo zigbuild --release --target i686-unknown-linux-gnu
 if errorlevel 1 goto :error
 
 copy /y target\x86_64-unknown-linux-gnu\release\lib%CARGO_NAME%.so "%OUTPUT_DIR%\AddIn_x64_linux.so"
@@ -102,6 +105,10 @@ if errorlevel 1 goto :error
 copy /y ..\MANIFEST_FOLV.XML "%OUTPUT_DIR%\MANIFEST.XML"
 if errorlevel 1 goto :error
 
+echo "FOLV ---" >> dependencies.log
+wsl -d OracleLinux_8_7 ldd %OUTPUT_DIR%/AddIn_x64_linux.so >> dependencies.log
+wsl -d OracleLinux_8_7 strings %OUTPUT_DIR%/AddIn_x64_linux.so | grep GLIBC_ >> dependencies.log
+
 :: Архивация
 powershell -Command "Compress-Archive -Path '%OUTPUT_DIR%\*' -Force -DestinationPath '%LIB_NAME%.zip'"
 if errorlevel 1 goto :error
@@ -115,6 +122,8 @@ copy /y "%LIB_NAME%.zip" "../../en/OPI/src/CommonTemplates/%LIB_NAME%/Template.a
 if exist "%OUTPUT_DIR%" (
     rmdir /S /Q "%OUTPUT_DIR%"
 )
+
+del "%LIB_NAME%.zip"
 
 @echo (FOLV) Build and packaging completed successfully.
 exit /b 0
