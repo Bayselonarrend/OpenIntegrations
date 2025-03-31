f762d468746119657461ad616ac00f66b53a868f
Anton Titovets <bayselonarrend@gmail.com>
2025-03-31 14:32:28 +0300

    Пересборка через wsl + zigbuild под glibc 2.18

--- src/addins/postgres/release.bat
+++ src/addins/postgres/release.bat
@@ -4,5 +4,6 @@
 set CARGO_NAME=opi_postgres
 set LIB_NAME=OPI_PostgreSQL
 set ADDIN_DIR=%~dp0
+set FOLV=true
 
 call ../build.bat
--- src/addins/tcp_client/release.bat
+++ src/addins/tcp_client/release.bat
@@ -4,5 +4,6 @@
 set CARGO_NAME=opi_tcpclient
 set LIB_NAME=OPI_TCPClient
 set ADDIN_DIR=%~dp0
+set FOLV=true
 
 call ../build.bat
