01b0ec778061f58dfb17936d8bfcab41f9962ac1
Anton Titovets <bayselonarrend@gmail.com>
2025-07-27 22:40:39 +0300

    FTP: Переработка структур

--- src/addins/ftp/src/component/tcp_establish.rs
+++ src/addins/ftp/src/component/tcp_establish.rs
@@ -1,56 +1,84 @@
 use std::io::{BufRead, BufReader, Write};
 use std::net::{SocketAddr, TcpStream};
-use crate::component::AddIn;
 use socks::{Socks4Stream, Socks5Stream};
 use base64::{Engine as _, engine::general_purpose};
+use suppaftp::FtpError;
+use crate::component::configuration::{FtpProxySettings, FtpSettings};
 
-pub fn create_tcp_connection(obj: &AddIn) -> Result<TcpStream, String> {
-    let target_addr = (obj.domain.as_str(), obj.port);
-    connect(obj, target_addr)
+pub fn make_passive_proxy_stream(
+    ftp_settings: &FtpSettings,
+    proxy_settings: &Option<FtpProxySettings>,
+    addr: SocketAddr) -> Result<TcpStream, FtpError> {
+
+    let corrected_addr = if addr.ip().is_loopback() {
+        match ftp_settings.domain.parse::<std::net::IpAddr>() {
+            Ok(ftp_ip) => {
+                SocketAddr::new(ftp_ip, addr.port())
+            },
+            Err(e) => {
+                return Err(FtpError::ConnectionError(
+                    std::io::Error::new(std::io::ErrorKind::Other, format!("Invalid FTP server IP '{}': {}", ftp_settings.domain, e))
+                ));
+            }
+        }
+    } else {
+        addr
+    };
+
+    create_tcp_connection_for_passive(&proxy_settings, corrected_addr)
+        .map_err(|e| FtpError::ConnectionError(
+            std::io::Error::new(std::io::ErrorKind::Other, e)))
 }
 
-pub fn create_tcp_connection_for_passive(obj: &AddIn, addr: SocketAddr) -> Result<TcpStream, String> {
+pub fn create_tcp_connection(
+    ftp_settings: &FtpSettings,
+    proxy_settings: &Option<FtpProxySettings>) -> Result<TcpStream, String> {
+
+    let target_addr = (ftp_settings.domain.as_str(), ftp_settings.port);
+    connect(proxy_settings, target_addr)
+}
+
+pub fn create_tcp_connection_for_passive(
+    proxy_settings: &Option<FtpProxySettings>,
+    addr: SocketAddr) -> Result<TcpStream, String> {
 
     let ip = addr.ip().to_string();
     let target_addr = (ip.as_str(), addr.port());
-    connect(obj, target_addr)
-
+    connect(proxy_settings, target_addr)
 }
 
 fn connect(
-    obj: &AddIn,
+    proxy_settings: &Option<FtpProxySettings>,
     target_addr: (&str, u16)
 ) -> Result<TcpStream, String> {
 
-    if let (Some(proxy_server), Some(proxy_port), Some(proxy_type)) =
-        (&obj.proxy_server, &obj.proxy_port, &obj.proxy_type) {
-
-        let proxy_addr = format!("{}:{}", proxy_server, proxy_port);
-        connect_via_proxy(obj, proxy_type, &proxy_addr, target_addr)
-
-    } else {
-        connect_direct(target_addr)
+    match proxy_settings{
+        Some(s) => connect_via_proxy(&s, target_addr),
+        None => connect_direct(target_addr)
     }
 }
 
 fn connect_via_proxy(
-    obj: &AddIn,
-    proxy_type: &str,
-    proxy_addr: &str,
+    proxy_settings: &FtpProxySettings,
     target_addr: (&str, u16)
 ) -> Result<TcpStream, String> {
+
+    let proxy_type = proxy_settings.proxy_type.as_str();
+
     match proxy_type.to_lowercase().as_str() {
-        "socks5" => connect_via_socks5(obj, proxy_addr, target_addr),
-        "socks4" => connect_via_socks4(obj, proxy_addr, target_addr),
-        "http" => connect_via_http_proxy(obj, proxy_addr, target_addr),
+        "socks5" => connect_via_socks5(proxy_settings, target_addr),
+        "socks4" => connect_via_socks4(proxy_settings, target_addr),
+        "http" => connect_via_http_proxy(proxy_settings, target_addr),
         _ => Err("Unsupported proxy type".to_string()),
     }
 }
 
 
-fn connect_via_socks5(obj: &AddIn, proxy_addr: &str, target_addr: (&str, u16)) -> Result<TcpStream, String> {
+fn connect_via_socks5(proxy_settings: &FtpProxySettings, target_addr: (&str, u16)) -> Result<TcpStream, String> {
 
-    let stream = if let (Some(user), Some(pass)) = (&obj.proxy_login, &obj.proxy_password) {
+    let proxy_addr = format!("{}:{}", proxy_settings.server, proxy_settings.port);
+
+    let stream = if let (Some(user), Some(pass)) = (&proxy_settings.login, &proxy_settings.password) {
         Socks5Stream::connect_with_password(proxy_addr, target_addr, user, pass)
     } else {
         Socks5Stream::connect(proxy_addr, target_addr)
@@ -60,9 +88,11 @@ fn connect_via_socks5(obj: &AddIn, proxy_addr: &str, target_addr: (&str, u16)) -
         .map_err(|e| format!("SOCKS5 error: {}", e))
 }
 
-fn connect_via_socks4(obj: &AddIn, proxy_addr: &str, target_addr: (&str, u16)) -> Result<TcpStream, String> {
+fn connect_via_socks4(proxy_settings: &FtpProxySettings, target_addr: (&str, u16)) -> Result<TcpStream, String> {
 
-    let stream = if let Some(user) = &obj.proxy_login {
+    let proxy_addr = format!("{}:{}", proxy_settings.server, proxy_settings.port);
+
+    let stream = if let Some(user) = &proxy_settings.login {
         Socks4Stream::connect(proxy_addr, target_addr, user)
     } else {
         Socks4Stream::connect(proxy_addr, target_addr, "")
@@ -72,11 +102,10 @@ fn connect_via_socks4(obj: &AddIn, proxy_addr: &str, target_addr: (&str, u16)) -
         .map_err(|e| format!("SOCKS4 error: {}", e))
 }
 
-fn connect_via_http_proxy(
-    obj: &AddIn,
-    proxy_addr: &str,
-    target_addr: (&str, u16),
-) -> Result<TcpStream, String> {
+fn connect_via_http_proxy(proxy_settings: &FtpProxySettings, target_addr: (&str, u16)) -> Result<TcpStream, String> {
+
+    let proxy_addr = format!("{}:{}", proxy_settings.server, proxy_settings.port);
+
     let mut stream = TcpStream::connect(proxy_addr)
         .map_err(|e| format!("Failed to connect to HTTP proxy: {}", e))?;
 
@@ -86,7 +115,7 @@ fn connect_via_http_proxy(
         host_port, host_port
     );
 
-    let request = if let (Some(user), Some(pass)) = (&obj.proxy_login, &obj.proxy_password) {
+    let request = if let (Some(user), Some(pass)) = (&proxy_settings.login, &proxy_settings.password) {
         let auth = general_purpose::STANDARD.encode(&format!("{}:{}", user, pass));
         format!("{}Proxy-Authorization: Basic {}\r\n\r\n", request, auth)
     } else {
@@ -135,3 +164,4 @@ fn connect_direct(addr: (&str, u16)) -> Result<TcpStream, String> {
     let target_addr = format!("{}:{}", addr.0, addr.1);
     TcpStream::connect(&target_addr).map_err(|e| format!("Direct connection error: {}", e))
 }
+
