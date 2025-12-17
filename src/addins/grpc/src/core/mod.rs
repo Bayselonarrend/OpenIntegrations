d65d1102c99cc4bc7d67c13ad717c027954552a7
Anton Titovets <bayselonarrend@gmail.com>
2025-12-17 11:16:46 +0300

    Фикс Call в gRPC, вынос бойлерплейта в макрос

--- src/addins/grpc/src/core/mod.rs
+++
@@ -1,8 +0,0 @@
-pub use common_core::getset;
-
-use common_core::impl_raw_addin;
-use crate::component::{AddIn, METHODS, PROPS, get_params_amount, cal_func};
-
-impl_raw_addin!(AddIn, METHODS, PROPS, get_params_amount, cal_func);
-
-
