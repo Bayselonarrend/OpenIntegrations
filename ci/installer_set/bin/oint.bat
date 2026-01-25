207da790866c7177b04b784d52f2083e58f682c8
Anton Titovets <bayselonarrend@gmail.com>
2026-01-25 12:13:56 +0300

    Доработки по переносу движка OS в /usr/lib

--- ci/installer_set/bin/oint.bat
+++ ci/installer_set/bin/oint.bat
@@ -1,4 +1,4 @@
 @echo off
 set "oint_folder=%~dp0"
-call "%oint_folder%..\share\oint\bin\oscript.exe" "%oint_folder%..\share\oint\lib\oint-cli\core\Classes\app.os" %*
+call "%oint_folder%..\lib\oint\bin\oscript.exe" "%oint_folder%..\share\oint\lib\oint-cli\core\Classes\app.os" %*
 @exit /b %ERRORLEVEL%
