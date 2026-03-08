7396cc0e092204f2fcc815d0d85c7df0bd01f3c7
Anton Titovets <bayselonarrend@gmail.com>
2026-03-08 17:55:12 +0300

    Начало TCP Server

---
+++ src/addins/tcp_server/src/backend.rs
@@ -0,0 +1,298 @@
+use std::sync::mpsc::{self, Sender};
+use std::thread::{self, JoinHandle};
+use common_utils::utils::{json_error, json_success};
+use crate::listener::ServerState;
+
+pub struct TcpServerBackend {
+    tx: Sender<BackendCommand>,
+    thread_handle: Option<JoinHandle<()>>,
+}
+
+enum BackendCommand {
+    Start {
+        port: u16,
+        queue_size: usize,
+        response: Sender<String>,
+    },
+    GetNextMessage {
+        timeout_ms: u64,
+        response: Sender<String>,
+    },
+    SendMessage {
+        connection_id: String,
+        message: Vec<u8>,
+        response: Sender<String>,
+    },
+    RemoveConnection {
+        connection_id: String,
+        response: Sender<String>,
+    },
+    CloseConnection {
+        connection_id: String,
+        response: Sender<String>,
+    },
+    ShutdownRead {
+        connection_id: String,
+        response: Sender<String>,
+    },
+    ShutdownWrite {
+        connection_id: String,
+        response: Sender<String>,
+    },
+    GetConnectionsList {
+        response: Sender<String>,
+    },
+    Shutdown,
+}
+
+impl TcpServerBackend {
+    pub fn new() -> Self {
+        let (tx, rx) = mpsc::channel();
+
+        let thread_handle = thread::Builder::new()
+            .name("opi_tcpserver_backend".to_string())
+            .spawn(move || {
+                let rt = tokio::runtime::Runtime::new().unwrap();
+                let mut server_state: Option<ServerState> = None;
+
+                while let Ok(cmd) = rx.recv() {
+                    match cmd {
+                        BackendCommand::Start { port, queue_size, response } => {
+                            if server_state.is_some() {
+                                let _ = response.send(json_error("Server already started"));
+                                continue;
+                            }
+
+                            let result = rt.block_on(async {
+                                ServerState::start(port, queue_size).await
+                            });
+
+                            match result {
+                                Ok(state) => {
+                                    server_state = Some(state);
+                                    let _ = response.send(json_success());
+                                }
+                                Err(e) => {
+                                    let _ = response.send(json_error(&e));
+                                }
+                            }
+                        }
+
+                        BackendCommand::GetNextMessage { timeout_ms, response } => {
+                            if let Some(ref mut state) = server_state {
+                                let result = rt.block_on(async {
+                                    state.get_next_message(timeout_ms).await
+                                });
+                                let _ = response.send(result);
+                            } else {
+                                let _ = response.send(json_error("Server not started"));
+                            }
+                        }
+
+                        BackendCommand::SendMessage { connection_id, message, response } => {
+                            if let Some(ref mut state) = server_state {
+                                let result = rt.block_on(async {
+                                    state.send_message(&connection_id, message).await
+                                });
+                                let _ = response.send(result);
+                            } else {
+                                let _ = response.send(json_error("Server not started"));
+                            }
+                        }
+
+                        BackendCommand::RemoveConnection { connection_id, response } => {
+                            if let Some(ref mut state) = server_state {
+                                let result = state.remove_connection(&connection_id);
+                                let _ = response.send(result);
+                            } else {
+                                let _ = response.send(json_error("Server not started"));
+                            }
+                        }
+
+                        BackendCommand::CloseConnection { connection_id, response } => {
+                            if let Some(ref mut state) = server_state {
+                                let result = rt.block_on(async {
+                                    state.close_connection(&connection_id).await
+                                });
+                                let _ = response.send(result);
+                            } else {
+                                let _ = response.send(json_error("Server not started"));
+                            }
+                        }
+
+                        BackendCommand::ShutdownRead { connection_id, response } => {
+                            if let Some(ref mut state) = server_state {
+                                let result = state.shutdown_read(&connection_id);
+                                let _ = response.send(result);
+                            } else {
+                                let _ = response.send(json_error("Server not started"));
+                            }
+                        }
+
+                        BackendCommand::ShutdownWrite { connection_id, response } => {
+                            if let Some(ref mut state) = server_state {
+                                let result = state.shutdown_write(&connection_id);
+                                let _ = response.send(result);
+                            } else {
+                                let _ = response.send(json_error("Server not started"));
+                            }
+                        }
+
+                        BackendCommand::GetConnectionsList { response } => {
+                            if let Some(ref mut state) = server_state {
+                                let result = state.get_connections_list();
+                                let _ = response.send(result);
+                            } else {
+                                let _ = response.send(json_error("Server not started"));
+                            }
+                        }
+
+                        BackendCommand::Shutdown => {
+                            server_state = None;
+                            break;
+                        }
+                    }
+                }
+            })
+            .unwrap();
+
+        Self {
+            tx,
+            thread_handle: Some(thread_handle),
+        }
+    }
+
+    pub fn start(&self, port: u16, queue_size: usize) -> String {
+        let (response_tx, response_rx) = mpsc::channel();
+
+        if let Err(e) = self.tx.send(BackendCommand::Start {
+            port,
+            queue_size,
+            response: response_tx,
+        }) {
+            return json_error(&format!("Failed to send command: {}", e));
+        }
+
+        response_rx
+            .recv()
+            .unwrap_or_else(|e| json_error(&format!("Failed to receive response: {}", e)))
+    }
+
+    pub fn get_next_message(&self, timeout_ms: u64) -> String {
+        let (response_tx, response_rx) = mpsc::channel();
+
+        if let Err(e) = self.tx.send(BackendCommand::GetNextMessage {
+            timeout_ms,
+            response: response_tx,
+        }) {
+            return json_error(&format!("Failed to send command: {}", e));
+        }
+
+        response_rx
+            .recv()
+            .unwrap_or_else(|e| json_error(&format!("Failed to receive response: {}", e)))
+    }
+
+    pub fn send_message(&self, connection_id: String, message: Vec<u8>) -> String {
+        let (response_tx, response_rx) = mpsc::channel();
+
+        if let Err(e) = self.tx.send(BackendCommand::SendMessage {
+            connection_id,
+            message,
+            response: response_tx,
+        }) {
+            return json_error(&format!("Failed to send command: {}", e));
+        }
+
+        response_rx
+            .recv()
+            .unwrap_or_else(|e| json_error(&format!("Failed to receive response: {}", e)))
+    }
+
+    pub fn remove_connection(&self, connection_id: String) -> String {
+        let (response_tx, response_rx) = mpsc::channel();
+
+        if let Err(e) = self.tx.send(BackendCommand::RemoveConnection {
+            connection_id,
+            response: response_tx,
+        }) {
+            return json_error(&format!("Failed to send command: {}", e));
+        }
+
+        response_rx
+            .recv()
+            .unwrap_or_else(|e| json_error(&format!("Failed to receive response: {}", e)))
+    }
+
+    pub fn close_connection(&self, connection_id: String) -> String {
+        let (response_tx, response_rx) = mpsc::channel();
+
+        if let Err(e) = self.tx.send(BackendCommand::CloseConnection {
+            connection_id,
+            response: response_tx,
+        }) {
+            return json_error(&format!("Failed to send command: {}", e));
+        }
+
+        response_rx
+            .recv()
+            .unwrap_or_else(|e| json_error(&format!("Failed to receive response: {}", e)))
+    }
+
+    pub fn shutdown_read(&self, connection_id: String) -> String {
+        let (response_tx, response_rx) = mpsc::channel();
+
+        if let Err(e) = self.tx.send(BackendCommand::ShutdownRead {
+            connection_id,
+            response: response_tx,
+        }) {
+            return json_error(&format!("Failed to send command: {}", e));
+        }
+
+        response_rx
+            .recv()
+            .unwrap_or_else(|e| json_error(&format!("Failed to receive response: {}", e)))
+    }
+
+    pub fn shutdown_write(&self, connection_id: String) -> String {
+        let (response_tx, response_rx) = mpsc::channel();
+
+        if let Err(e) = self.tx.send(BackendCommand::ShutdownWrite {
+            connection_id,
+            response: response_tx,
+        }) {
+            return json_error(&format!("Failed to send command: {}", e));
+        }
+
+        response_rx
+            .recv()
+            .unwrap_or_else(|e| json_error(&format!("Failed to receive response: {}", e)))
+    }
+
+    pub fn get_connections_list(&self) -> String {
+        let (response_tx, response_rx) = mpsc::channel();
+
+        if let Err(e) = self.tx.send(BackendCommand::GetConnectionsList {
+            response: response_tx,
+        }) {
+            return json_error(&format!("Failed to send command: {}", e));
+        }
+
+        response_rx
+            .recv()
+            .unwrap_or_else(|e| json_error(&format!("Failed to receive response: {}", e)))
+    }
+
+    pub fn shutdown(&mut self) {
+        let _ = self.tx.send(BackendCommand::Shutdown);
+        if let Some(handle) = self.thread_handle.take() {
+            let _ = handle.join();
+        }
+    }
+}
+
+impl Drop for TcpServerBackend {
+    fn drop(&mut self) {
+        self.shutdown();
+    }
+}
---
+++ src/addins/tcp_server/src/lib.rs
@@ -0,0 +1,222 @@
+mod backend;
+mod listener;
+
+use std::sync::{Arc, Mutex};
+use common_core::*;
+use common_utils::utils::{json_error, json_success};
+use crate::backend::TcpServerBackend;
+
+impl_addin_exports!(AddIn);
+impl_raw_addin!(AddIn, METHODS, PROPS, get_params_amount, cal_func);
+
+pub const METHODS: &[&[u16]] = &[
+    name!("Start"),                 // 0
+    name!("Stop"),                  // 1
+    name!("GetNextMessage"),        // 2
+    name!("SendMessage"),           // 3
+    name!("RemoveConnection"),      // 4
+    name!("CloseConnection"),       // 5
+    name!("ShutdownRead"),          // 6
+    name!("ShutdownWrite"),         // 7
+    name!("GetConnectionsList"),    // 8
+];
+
+pub fn get_params_amount(num: usize) -> usize {
+    match num {
+        0 => 2,  // Start(port, queue_size)
+        1 => 0,  // Stop()
+        2 => 1,  // GetNextMessage(timeout_ms)
+        3 => 2,  // SendMessage(connection_id, message)
+        4 => 1,  // RemoveConnection(connection_id)
+        5 => 1,  // CloseConnection(connection_id)
+        6 => 1,  // ShutdownRead(connection_id)
+        7 => 1,  // ShutdownWrite(connection_id)
+        8 => 0,  // GetConnectionsList()
+        _ => 0,
+    }
+}
+
+pub fn cal_func(obj: &mut AddIn, num: usize, params: &mut [Variant]) -> Box<dyn getset::ValueType> {
+    let empty_array: [u8; 0] = [];
+
+    match num {
+        0 => {
+            let port = params[0].get_i32().unwrap_or(0) as u16;
+            let queue_size = params[1].get_i32().unwrap_or(10) as usize;
+            Box::new(obj.start_server(port, queue_size))
+        },
+        1 => {
+            Box::new(obj.stop_server())
+        },
+        2 => {
+            let timeout_ms = params[0].get_i32().unwrap_or(1000) as u64;
+            Box::new(obj.get_next_message(timeout_ms))
+        },
+        3 => {
+            let connection_id = params[0].get_string().unwrap_or_default();
+            let message = params[1].get_blob().unwrap_or(&empty_array);
+            Box::new(obj.send_message(&connection_id, message.to_vec()))
+        },
+        4 => {
+            let connection_id = params[0].get_string().unwrap_or_default();
+            Box::new(obj.remove_connection(&connection_id))
+        },
+        5 => {
+            let connection_id = params[0].get_string().unwrap_or_default();
+            Box::new(obj.close_connection(&connection_id))
+        },
+        6 => {
+            let connection_id = params[0].get_string().unwrap_or_default();
+            Box::new(obj.shutdown_read(&connection_id))
+        },
+        7 => {
+            let connection_id = params[0].get_string().unwrap_or_default();
+            Box::new(obj.shutdown_write(&connection_id))
+        },
+        8 => {
+            Box::new(obj.get_connections_list())
+        },
+        _ => Box::new(false),
+    }
+}
+
+pub const PROPS: &[&[u16]] = &[];
+
+pub struct AddIn {
+    backend: Arc<Mutex<TcpServerBackend>>,
+    started: bool,
+}
+
+impl AddIn {
+    pub fn new() -> Self {
+        AddIn {
+            backend: Arc::new(Mutex::new(TcpServerBackend::new())),
+            started: false,
+        }
+    }
+
+    pub fn start_server(&mut self, port: u16, queue_size: usize) -> String {
+        if self.started {
+            return json_error("Server already started");
+        }
+
+        let result = match self.backend.lock() {
+            Ok(backend) => backend.start(port, queue_size),
+            Err(e) => json_error(&format!("Failed to lock backend: {}", e)),
+        };
+
+        if result.contains("\"result\":true") {
+            self.started = true;
+        }
+
+        result
+    }
+
+    pub fn stop_server(&mut self) -> String {
+        if !self.started {
+            return json_error("Server not started");
+        }
+
+        match self.backend.lock() {
+            Ok(mut backend) => {
+                backend.shutdown();
+                self.started = false;
+                json_success()
+            }
+            Err(e) => json_error(&format!("Failed to lock backend: {}", e)),
+        }
+    }
+
+    pub fn get_next_message(&self, timeout_ms: u64) -> String {
+        if !self.started {
+            return json_error("Server not started");
+        }
+
+        match self.backend.lock() {
+            Ok(backend) => backend.get_next_message(timeout_ms),
+            Err(e) => json_error(&format!("Failed to lock backend: {}", e)),
+        }
+    }
+
+    pub fn send_message(&self, connection_id: &str, message: Vec<u8>) -> String {
+        if !self.started {
+            return json_error("Server not started");
+        }
+
+        match self.backend.lock() {
+            Ok(backend) => backend.send_message(connection_id.to_string(), message),
+            Err(e) => json_error(&format!("Failed to lock backend: {}", e)),
+        }
+    }
+
+    pub fn remove_connection(&self, connection_id: &str) -> String {
+        if !self.started {
+            return json_error("Server not started");
+        }
+
+        match self.backend.lock() {
+            Ok(backend) => backend.remove_connection(connection_id.to_string()),
+            Err(e) => json_error(&format!("Failed to lock backend: {}", e)),
+        }
+    }
+
+    pub fn close_connection(&self, connection_id: &str) -> String {
+        if !self.started {
+            return json_error("Server not started");
+        }
+
+        match self.backend.lock() {
+            Ok(backend) => backend.close_connection(connection_id.to_string()),
+            Err(e) => json_error(&format!("Failed to lock backend: {}", e)),
+        }
+    }
+
+    pub fn shutdown_read(&self, connection_id: &str) -> String {
+        if !self.started {
+            return json_error("Server not started");
+        }
+
+        match self.backend.lock() {
+            Ok(backend) => backend.shutdown_read(connection_id.to_string()),
+            Err(e) => json_error(&format!("Failed to lock backend: {}", e)),
+        }
+    }
+
+    pub fn shutdown_write(&self, connection_id: &str) -> String {
+        if !self.started {
+            return json_error("Server not started");
+        }
+
+        match self.backend.lock() {
+            Ok(backend) => backend.shutdown_write(connection_id.to_string()),
+            Err(e) => json_error(&format!("Failed to lock backend: {}", e)),
+        }
+    }
+
+    pub fn get_connections_list(&self) -> String {
+        if !self.started {
+            return json_error("Server not started");
+        }
+
+        match self.backend.lock() {
+            Ok(backend) => backend.get_connections_list(),
+            Err(e) => json_error(&format!("Failed to lock backend: {}", e)),
+        }
+    }
+
+    pub fn get_field_ptr(&self, _index: usize) -> *const dyn getset::ValueType {
+        panic!("Index out of bounds")
+    }
+
+    pub fn get_field_ptr_mut(&mut self, index: usize) -> *mut dyn getset::ValueType {
+        self.get_field_ptr(index) as *mut _
+    }
+}
+
+impl Drop for AddIn {
+    fn drop(&mut self) {
+        if self.started {
+            let _ = self.stop_server();
+        }
+    }
+}
