aa2d0f0159afdf4d3b2904c8f836dc266e217368
Anton Titovets <bayselonarrend@gmail.com>
2026-03-21 19:49:05 +0300

    Пересборка компонент

--- src/addins/mysql/src/lib.rs
+++ src/addins/mysql/src/lib.rs
@@ -1,13 +1,13 @@
 mod methods;
 
-use serde_json::json;
-use mysql::*;
-use std::path::PathBuf;
+use common_binary::vault::{BinaryInput, BinaryVault};
+use common_core::*;
 use common_dataset::dataset::Datasets;
 use common_tcp::tls_settings::TlsSettings;
 use common_utils::utils::{json_error, json_success};
-use common_binary::vault::{BinaryInput, BinaryVault};
-use common_core::*;
+use mysql::*;
+use serde_json::json;
+use std::path::PathBuf;
 
 impl_addin_exports!(AddIn);
 impl_raw_addin!(AddIn, METHODS, PROPS, get_params_amount, cal_func);
@@ -52,113 +52,122 @@ pub fn get_params_amount(num: usize) -> usize {
 }
 
 pub fn cal_func(obj: &mut AddIn, num: usize, params: &mut [Variant]) -> Box<dyn getset::ValueType> {
-
     let empty_array: [u8; 0] = [];
 
     match num {
-
         0 => Box::new(obj.initialize()),
         1 => Box::new(obj.close_connection()),
         2 => {
             let key = params[0].get_string().unwrap_or("".to_string());
             Box::new(obj.execute_query(&key))
-        },
+        }
         3 => {
             let use_tls = params[0].get_bool().unwrap_or(false);
             let accept_invalid_certs = params[1].get_bool().unwrap_or(false);
             let ca_cert_path = params[2].get_string().unwrap_or("".to_string());
-            if obj.connections.is_some(){
-                return  Box::new(json_error("TLS settings can only be set before the connection is established"));
+            if obj.connections.is_some() {
+                return Box::new(json_error(
+                    "TLS settings can only be set before the connection is established",
+                ));
             };
 
-            obj.tls = Some(TlsSettings::new(use_tls, accept_invalid_certs, &ca_cert_path));
+            obj.tls = Some(TlsSettings::new(
+                use_tls,
+                accept_invalid_certs,
+                &ca_cert_path,
+            ));
             Box::new(json_success())
-        },
+        }
         4 => {
             let text = params[0].get_string().unwrap_or("".to_string());
             let force = params[1].get_bool().unwrap_or(false);
             let from_file = params[2].get_bool().unwrap_or(false);
-            let result = match obj.datasets.init_query(&text, force, from_file){
+            let result = match obj.datasets.init_query(&text, force, from_file) {
                 Ok(key) => json!({"result": true, "key": key}).to_string(),
-                Err(e) => json_error(&e)
+                Err(e) => json_error(&e),
             };
             Box::new(result)
-        },
+        }
         5 => {
             let key = params[0].get_string().unwrap_or("".to_string());
             let filepath = params[1].get_string().unwrap_or("".to_string());
-            let result = match obj.datasets.result_as_file(&key, &filepath){
+            let result = match obj.datasets.result_as_file(&key, &filepath) {
                 Ok(_) => json_success(),
-                Err(e) => json_error(&e)
+                Err(e) => json_error(&e),
             };
             Box::new(result)
-        },
+        }
         6 => {
             let key = params[0].get_string().unwrap_or("".to_string());
-            let result = obj.datasets.result_as_string(&key)
+            let result = obj
+                .datasets
+                .result_as_string(&key)
                 .unwrap_or_else(|e| json_error(&e));
             Box::new(result)
-        },
+        }
         7 => {
             let key = params[0].get_string().unwrap_or("".to_string());
             let filepath = params[1].get_string().unwrap_or("".to_string());
-            let result = match obj.datasets.params_from_file(&key, &filepath){
+            let result = match obj.datasets.params_from_file(&key, &filepath) {
                 Ok(_) => json_success(),
-                Err(e) => json_error(&e)
+                Err(e) => json_error(&e),
             };
             Box::new(result)
-        },
+        }
         8 => {
             let key = params[0].get_string().unwrap_or("".to_string());
             let json = params[1].get_string().unwrap_or("".to_string());
-            let result = match obj.datasets.params_from_string(&key, &json){
+            let result = match obj.datasets.params_from_string(&key, &json) {
                 Ok(_) => json_success(),
-                Err(e) => json_error(&e)
+                Err(e) => json_error(&e),
             };
             Box::new(result)
-        },
+        }
         9 => {
             let key = params[0].get_string().unwrap_or("".to_string());
             obj.datasets.remove(&key);
             Box::new(json_success())
-        },
+        }
         10 => {
             let input = params[0].get_string().unwrap_or("".to_string());
             let output = params[1].get_string().unwrap_or("".to_string());
-            let result = match obj.datasets.batch_query_init(&input, &output){
+            let result = match obj.datasets.batch_query_init(&input, &output) {
                 Ok(_) => json_success(),
-                Err(e) => json_error(&e)
+                Err(e) => json_error(&e),
             };
             Box::new(result)
-        },
+        }
         11 => {
-            let result = match &obj.tls{
+            let result = match &obj.tls {
                 Some(tls) => tls.get_settings(),
-                None => "".to_string()
+                None => "".to_string(),
             };
             Box::new(result)
-        },
+        }
         12 => {
             let binary = params[0].get_blob().unwrap_or(&empty_array);
-            let result = match obj.binary_vault.store(BinaryInput::Bytes(Vec::from(binary))){
+            let result = match obj
+                .binary_vault
+                .store(BinaryInput::Bytes(Vec::from(binary)))
+            {
                 Ok(key) => json!({"result": true, "key": key}).to_string(),
-                Err(e) => json_error(&e)
+                Err(e) => json_error(&e),
             };
             Box::new(result)
-        },
+        }
         13 => {
             let file = params[0].get_string().unwrap_or("".to_string());
-            let result = match obj.binary_vault.store(BinaryInput::FilePath(file)){
+            let result = match obj.binary_vault.store(BinaryInput::FilePath(file)) {
                 Ok(key) => json!({"result": true, "key": key}).to_string(),
-                Err(e) => json_error(&e)
+                Err(e) => json_error(&e),
             };
             Box::new(result)
-        },
+        }
         14 => {
             let base64 = params[0].get_string().unwrap_or("".to_string());
-            let result = match obj.binary_vault.store(BinaryInput::Base64(base64)){
+            let result = match obj.binary_vault.store(BinaryInput::Base64(base64)) {
                 Ok(key) => json!({"result": true, "key": key}).to_string(),
-                Err(e) => json_error(&e)
+                Err(e) => json_error(&e),
             };
             Box::new(result)
         }
@@ -166,9 +175,7 @@ pub fn cal_func(obj: &mut AddIn, num: usize, params: &mut [Variant]) -> Box<dyn 
     }
 }
 
-pub const PROPS: &[&[u16]] = &[
-    name!("ConnectionString")
-];
+pub const PROPS: &[&[u16]] = &[name!("ConnectionString")];
 
 pub struct AddIn {
     connection_string: String,
@@ -176,7 +183,7 @@ pub struct AddIn {
     tls: Option<TlsSettings>,
     connection: Option<PooledConn>,
     datasets: Datasets,
-    binary_vault: BinaryVault
+    binary_vault: BinaryVault,
 }
 
 impl AddIn {
@@ -187,28 +194,27 @@ impl AddIn {
             tls: None,
             connection: None,
             datasets: Datasets::new(),
-            binary_vault: BinaryVault::new()
+            binary_vault: BinaryVault::new(),
         }
     }
     pub fn initialize(&mut self) -> String {
-
         let connection_string = if self.connection_string.is_empty() {
-            return json_error("Empty connection string!")
+            return json_error("Empty connection string!");
         } else {
             &self.connection_string
         };
 
-        let opts = match Opts::from_url(connection_string){
+        let opts = match Opts::from_url(connection_string) {
             Ok(opts) => opts,
-            Err(e) => return json_error(e.to_string().as_str())
+            Err(e) => return json_error(e.to_string().as_str()),
         };
 
         let mut opts_builder = OptsBuilder::from_opts(opts);
 
         if let Some(tls) = &self.tls {
-            if tls.enabled(){
-                let mut ssl_opts = SslOpts::default()
-                    .with_danger_accept_invalid_certs(tls.accept_invalid_certs);
+            if tls.enabled() {
+                let mut ssl_opts =
+                    SslOpts::default().with_danger_accept_invalid_certs(tls.accept_invalid_certs);
 
                 if !tls.ca_cert_path.is_empty() {
                     let cert_path: PathBuf = tls.ca_cert_path.clone().into();
@@ -219,17 +225,16 @@ impl AddIn {
             }
         }
 
-        match Pool::new(opts_builder){
+        match Pool::new(opts_builder) {
             Ok(p) => {
-
                 self.connections = Some(p);
-                self.connection = match self.get_connection(){
+                self.connection = match self.get_connection() {
                     Ok(p) => Some(p),
-                    Err(e) => return json_error(&e)
+                    Err(e) => return json_error(&e),
                 };
                 json_success()
-            },
-            Err(e) => json_error(&e)
+            }
+            Err(e) => json_error(&e),
         }
     }
 
@@ -251,8 +256,7 @@ impl AddIn {
         }
     }
 
-    fn get_connection(&mut self) -> Result<PooledConn, String>{
-
+    fn get_connection(&mut self) -> Result<PooledConn, String> {
         if let Some(mut conn) = self.connection.take() {
             if conn.as_mut().ping().is_ok() {
                 return Ok(conn);
@@ -263,12 +267,12 @@ impl AddIn {
 
         let pool = match self.connections {
             Some(ref mut conns) => conns,
-            None => return Err(json_error("No connections pool!"))
+            None => return Err(json_error("No connections pool!")),
         };
 
-        match pool.get_conn(){
+        match pool.get_conn() {
             Ok(conn) => Ok(conn),
-            Err(e) => Err(json_error(&e))
+            Err(e) => Err(json_error(&e)),
         }
     }
 
@@ -278,7 +282,9 @@ impl AddIn {
             _ => panic!("Index out of bounds"),
         }
     }
-    pub fn get_field_ptr_mut(&mut self, index: usize) -> *mut dyn getset::ValueType { self.get_field_ptr(index) as *mut _ }
+    pub fn get_field_ptr_mut(&mut self, index: usize) -> *mut dyn getset::ValueType {
+        self.get_field_ptr(index) as *mut _
+    }
 }
 
 impl Drop for AddIn {
