e8946414510d3d85a07f5f4aaae813564460725a
Anton Titovets <bayselonarrend@gmail.com>
2026-04-16 19:48:40 +0300

    Доработка методов работы с соединениями ws_server

--- src/addins/commons/common-utils/src/utils.rs
+++ src/addins/commons/common-utils/src/utils.rs
@@ -1,4 +1,5 @@
 use serde_json::json;
+use std::sync::{Mutex, MutexGuard};
 
 pub fn json_error<E: ToString>(error: E) -> String {
     let err = error.to_string();
@@ -7,4 +8,8 @@ pub fn json_error<E: ToString>(error: E) -> String {
 
 pub fn json_success() -> String {
     json!({"result": true}).to_string()
+}
+
+pub fn lock_unpoisoned<T>(mutex: &Mutex<T>) -> MutexGuard<'_, T> {
+    mutex.lock().unwrap_or_else(|poisoned| poisoned.into_inner())
 }
--- src/addins/ws_server/src/server/connections.rs
+++ src/addins/ws_server/src/server/connections.rs
@@ -1,13 +1,13 @@
 use super::{OutgoingMessage, WebSocketServerState};
 use common_server::MessageHandler;
+use common_utils::utils::lock_unpoisoned;
 use serde_json::json;
 
 impl WebSocketServerState {
     pub(crate) fn close_all_connections_internal(&mut self) {
-        let mut manager = match self.manager.lock() {
-            Ok(guard) => guard,
-            Err(poisoned) => poisoned.into_inner(),
-        };
+        
+        let mut manager = lock_unpoisoned(&self.manager);
+
         manager.iter_mut(|_, conn| {
             conn.is_closed = true;
             let _ = conn.outgoing_tx.send(OutgoingMessage::Close);
@@ -15,35 +15,36 @@ impl WebSocketServerState {
         manager.clear();
     }
 
-    pub fn close_connection(&mut self, connection_id: &str) -> String {
-        let mut manager = match self.manager.lock() {
-            Ok(guard) => guard,
-            Err(poisoned) => poisoned.into_inner(),
-        };
+    pub fn close_connection(&mut self, connection_id: &str, remove_from_list: bool) -> String {
+        let mut manager = lock_unpoisoned(&self.manager);
+
         if let Some(close_sent) = manager.get_mut(connection_id, |conn| {
             conn.is_closed = true;
             conn.outgoing_tx.send(OutgoingMessage::Close).is_ok()
         }) {
-            self.log(&format!("WebSocket closed: {}", connection_id));
-            if close_sent {
-                json!({
-                    "result": true,
-                    "message": "WebSocket closed"
-                }).to_string()
-            } else {
-                MessageHandler::error_response("Failed to send close frame")
+            if remove_from_list {
+                let _ = manager.remove(connection_id);
             }
+
+            self.log(&format!(
+                "WebSocket closed: {} (remove_from_list={}, close_sent={})",
+                connection_id, remove_from_list, close_sent
+            ));
+
+            json!({
+                "result": true,
+                "message": "WebSocket closed"
+            })
+            .to_string()
         } else {
             MessageHandler::error_response("WebSocket connection not found")
         }
     }
 
     pub fn get_connections_list(&mut self) -> String {
-        let mut manager = match self.manager.lock() {
-            Ok(guard) => guard,
-            Err(poisoned) => poisoned.into_inner(),
-        };
+
         let mut connections_list = Vec::new();
+        let mut manager = lock_unpoisoned(&self.manager);
 
         manager.iter_mut(|conn_id, conn_info| {
             connections_list.push(json!({
