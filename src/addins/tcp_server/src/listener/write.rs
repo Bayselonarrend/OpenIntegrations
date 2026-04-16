0df5c12893d572faab7df386733452db577da150
Anton Titovets <bayselonarrend@gmail.com>
2026-04-16 19:48:40 +0300

    Доработка методов работы с соединениями ws_server

--- src/addins/tcp_server/src/listener/write.rs
+++ src/addins/tcp_server/src/listener/write.rs
@@ -1,6 +1,6 @@
 use super::ServerState;
 use tokio::io::AsyncWriteExt;
-use common_utils::utils::{json_error, json_success};
+use common_utils::utils::{json_error, json_success, lock_unpoisoned};
 
 impl ServerState {
     pub async fn send_message(&mut self, connection_id: &str, message: Vec<u8>) -> String {
@@ -9,7 +9,7 @@ impl ServerState {
         // Actually perform the write
         let write_result = {
             let connections_arc = self.manager.connections_arc();
-            let mut conns = connections_arc.lock().unwrap_or_else(|p| p.into_inner());
+            let mut conns = lock_unpoisoned(&connections_arc);
             
             if let Some(conn) = conns.get_mut(connection_id) {
                 if let Some(ref mut write_half) = conn.write_half {
