cad2abccda7695f42890ff887e062a1111e14d8e
Anton Titovets <bayselonarrend@gmail.com>
2026-05-04 21:46:30 +0300

    Доработка Subscribe в ZMQ

--- src/addins/zeromq/src/lib.rs
+++ src/addins/zeromq/src/lib.rs
@@ -9,23 +9,25 @@ impl_addin_exports!(AddIn);
 impl_raw_addin!(AddIn, METHODS, PROPS, get_params_amount, cal_func);
 
 pub const METHODS: &[&[u16]] = &[
-    name!("Connect"),                // 0
-    name!("Bind"),                   // 1
-    name!("Subscribe"),              // 2
-    name!("Send"),                   // 3
-    name!("Recv"),                   // 4
-    name!("Close"),                  // 5
-    name!("RetrieveBinaryFromVault"), // 6
+    name!("Connect"),
+    name!("Bind"),
+    name!("Subscribe"),
+    name!("Send"),                    // 3
+    name!("Recv"),                    // 4
+    name!("Close"),                   // 5
+    name!("RetrieveBinaryFromVault"),
+    name!("BindPub"),
+    name!("ConnectSub"),
 ];
 
 pub fn get_params_amount(num: usize) -> usize {
     match num {
-        0 | 1 => 1, // Connect(endpoint) / Bind(endpoint)
-        2 => 1,     // Subscribe
-        3 => 2,     // Send(data, flags)
-        4 => 1,     // Recv
-        5 => 0,     // Close
-        6 => 1,     // RetrieveBinaryFromVault
+        0 | 1 | 7 | 8 => 1, // Connect/Bind/BindPub/ConnectSub(endpoint)
+        2 => 1,             // Subscribe(prefix)
+        3 => 2,             // Send(data, flags)
+        4 => 1,             // Recv
+        5 => 0,             // Close
+        6 => 1,             // RetrieveBinaryFromVault
         _ => 0,
     }
 }
@@ -63,6 +65,14 @@ pub fn cal_func(
         6 => {
             let key = params[0].get_string().unwrap_or_default();
             Box::new(obj.retrieve_binary_from_vault(&key))
+        }
+        7 => {
+            let ep = params[0].get_string().unwrap_or_default();
+            Box::new(obj.bind_pub(&ep))
+        }
+        8 => {
+            let ep = params[0].get_string().unwrap_or_default();
+            Box::new(obj.connect_sub(&ep))
         }
         _ => Box::new(false),
     }
