a0324616fd98c011570a9a761a82d26f7fdc67de
Anton Titovets <bayselonarrend@gmail.com>
2026-05-23 21:01:03 +0300

    Рефакторинг ws-client

--- src/addins/ws_client/src/lib.rs
+++ src/addins/ws_client/src/lib.rs
@@ -1,49 +1,53 @@
+mod addin;
+mod backend;
 mod client;
 mod connection;
-mod sending;
 mod receiving;
+mod sending;
+mod worker;
 
-use std::sync::{Arc, Mutex};
+use addin::AddIn;
 use common_core::*;
-use common_utils::utils::{json_error, version};
-use common_binary::vault::BinaryVault;
-use common_logs::Logger;
-use client::WebSocketClient;
+use common_utils::utils::version;
 
 impl_addin_exports!(AddIn);
 impl_raw_addin!(AddIn, METHODS, PROPS, get_params_amount, cal_func);
 
 pub const METHODS: &[&[u16]] = &[
-    name!("Connect"),                    // 0
-    name!("SendText"),                   // 1
-    name!("SendBinary"),                 // 2
-    name!("ReceiveMessage"),             // 3
-    name!("SendPing"),                   // 4
-    name!("SendPong"),                   // 5
-    name!("Close"),                      // 6
-    name!("SetHeaders"),                 // 7
-    name!("RetrieveBinaryFromVault"),    // 8
-    name!("GetLogs"),                    // 9
-    name!("SetTLS"),                     // 10
-    name!("SetProxySettings"),           // 11
+    name!("Connect"),
+    name!("SendText"),
+    name!("SendBinary"),
+    name!("ReceiveMessage"),
+    name!("SendPing"),
+    name!("SendPong"),
+    name!("Close"),
+    name!("SetHeaders"),
+    name!("RetrieveBinaryFromVault"),
+    name!("SetLogger"),
+    name!("GetLogs"),
+    name!("SetTLS"),
+    name!("SetProxySettings"),
     name!("Version"),
 ];
 
+pub const PROPS: &[&[u16]] = &[];
+
 pub fn get_params_amount(num: usize) -> usize {
     match num {
-        0 => 2,  // Connect(url, logger_config_json)
-        1 => 1,  // SendText(text)
-        2 => 1,  // SendBinary(data)
-        3 => 1,  // ReceiveMessage(timeout_ms)
-        4 => 0,  // SendPing()
-        5 => 0,  // SendPong()
-        6 => 2,  // Close(code, reason)
-        7 => 1,  // SetHeaders(headers_json)
-        8 => 1,  // RetrieveBinaryFromVault(vault_key)
-        9 => 1,  // GetLogs(count)
-        10 => 3, // SetTLS(use_tls, accept_invalid_certs, ca_cert_path)
-        11 => 1, // SetProxySettings(proxy_json)
-        12 => 0,
+        0 => 1,
+        1 => 1,
+        2 => 1,
+        3 => 1,
+        4 => 0,
+        5 => 0,
+        6 => 2,
+        7 => 1,
+        8 => 1,
+        9 => 1,
+        10 => 1,
+        11 => 3,
+        12 => 1,
+        13 => 0,
         _ => 0,
     }
 }
@@ -54,177 +58,54 @@ pub fn cal_func(obj: &mut AddIn, num: usize, params: &mut [Variant]) -> Box<dyn 
     match num {
         0 => {
             let url = params[0].get_string().unwrap_or_default();
-            let logger_config = params[1].get_string().unwrap_or_default();
-
-            if let Err(e) = obj.init_logger_if_needed(&logger_config) {
-                return Box::new(json_error(&e));
-            };
-            
-            let mut client = match obj.lock_client() {
-                Ok(c) => c,
-                Err(e) => return Box::new(json_error(&e)),
-            };
-            Box::new(client.connect(&url))
-        },
+            Box::new(obj.connect(&url))
+        }
         1 => {
             let text = params[0].get_string().unwrap_or_default();
-            let mut client = match obj.lock_client() {
-                Ok(c) => c,
-                Err(e) => return Box::new(json_error(&e)),
-            };
-            Box::new(client.send_text(&text))
-        },
+            Box::new(obj.send_text(&text))
+        }
         2 => {
             let data = params[0].get_blob().unwrap_or(&empty_array);
-            let mut client = match obj.lock_client() {
-                Ok(c) => c,
-                Err(e) => return Box::new(json_error(&e)),
-            };
-            Box::new(client.send_binary(data.to_vec()))
-        },
+            Box::new(obj.send_binary(data.to_vec()))
+        }
         3 => {
             let timeout_ms = params[0].get_i32().unwrap_or(1000) as u64;
-            let mut client = match obj.lock_client() {
-                Ok(c) => c,
-                Err(e) => return Box::new(json_error(&e)),
-            };
-            Box::new(client.receive_message(timeout_ms))
-        },
-        4 => {
-            let mut client = match obj.lock_client() {
-                Ok(c) => c,
-                Err(e) => return Box::new(json_error(&e)),
-            };
-            Box::new(client.send_ping())
-        },
-        5 => {
-            let mut client = match obj.lock_client() {
-                Ok(c) => c,
-                Err(e) => return Box::new(json_error(&e)),
-            };
-            Box::new(client.send_pong())
-        },
+            Box::new(obj.receive_message(timeout_ms))
+        }
+        4 => Box::new(obj.send_ping()),
+        5 => Box::new(obj.send_pong()),
         6 => {
             let code = params[0].get_i32().unwrap_or(1000) as u16;
             let reason = params[1].get_string().unwrap_or_default();
-            let mut client = match obj.lock_client() {
-                Ok(c) => c,
-                Err(e) => return Box::new(json_error(&e)),
-            };
-            Box::new(client.close(code, &reason))
-        },
+            Box::new(obj.close(code, &reason))
+        }
         7 => {
             let headers_json = params[0].get_string().unwrap_or_default();
-            let mut client = match obj.lock_client() {
-                Ok(c) => c,
-                Err(e) => return Box::new(json_error(&e)),
-            };
-            Box::new(client.set_headers(&headers_json))
-        },
+            Box::new(obj.set_headers(&headers_json))
+        }
         8 => {
             let vault_key = params[0].get_string().unwrap_or_default();
             Box::new(obj.retrieve_binary_from_vault(&vault_key))
-        },
+        }
         9 => {
+            let logger_config = params[0].get_string().unwrap_or_default();
+            Box::new(obj.set_logger(&logger_config))
+        }
+        10 => {
             let count = params[0].get_i32().unwrap_or(0) as usize;
             Box::new(obj.get_logs(count))
-        },
-        10 => {
+        }
+        11 => {
             let use_tls = params[0].get_bool().unwrap_or(false);
             let accept_invalid_certs = params[1].get_bool().unwrap_or(false);
             let ca_cert_path = params[2].get_string().unwrap_or_default();
-            let mut client = match obj.lock_client() {
-                Ok(c) => c,
-                Err(e) => return Box::new(json_error(&e)),
-            };
-            Box::new(client.set_tls(use_tls, accept_invalid_certs, &ca_cert_path))
-        },
-        11 => {
+            Box::new(obj.set_tls(use_tls, accept_invalid_certs, &ca_cert_path))
+        }
+        12 => {
             let proxy_json = params[0].get_string().unwrap_or_default();
-            let mut client = match obj.lock_client() {
-                Ok(c) => c,
-                Err(e) => return Box::new(json_error(&e)),
-            };
-            Box::new(client.set_proxy(&proxy_json))
-        },
-        12 => Box::new(version()),
+            Box::new(obj.set_proxy(&proxy_json))
+        }
+        13 => Box::new(version()),
         _ => Box::new(false),
-    }
-}
-
-pub const PROPS: &[&[u16]] = &[];
-
-pub struct AddIn {
-    client: Arc<Mutex<WebSocketClient>>,
-    vault: BinaryVault,
-    logger: Option<Arc<Logger>>,
-}
-
-impl AddIn {
-    fn lock_client(&self) -> Result<std::sync::MutexGuard<'_, WebSocketClient>, String> {
-        self.client.lock().map_err(|e| format!("Client lock failed: {}", e))
-    }
-
-    pub fn new() -> Self {
-        let vault = BinaryVault::new();
-        AddIn {
-            client: Arc::new(Mutex::new(WebSocketClient::new(vault.clone()))),
-            vault,
-            logger: None,
-        }
-    }
-
-    fn init_logger_if_needed(&mut self, logger_config: &str) -> Result<(), String> {
-        if logger_config.is_empty() || self.logger.is_some() {
-            return Ok(());
-        }
-
-        let logger = Logger::from_json(logger_config)
-            .map_err(|e| format!("Failed to initialize logger: {}", e))?;
-        
-        let logger_arc = Arc::new(logger);
-        self.logger = Some(logger_arc.clone());
-        
-        let mut client = self.lock_client()
-            .map_err(|e| format!("Failed to set logger to client: {}", e))?;
-        client.set_logger(logger_arc);
-
-        Ok(())
-    }
-
-    pub fn get_logs(&self, count: usize) -> String {
-        if let Some(ref logger) = self.logger {
-            let logs = logger.get_last_logs(count);
-            let total = logger.len();
-            
-            serde_json::json!({
-                "result": true,
-                "logs": logs,
-                "total": total,
-                "returned": logs.len()
-            }).to_string()
-        } else {
-            json_error("Logger not initialized")
-        }
-    }
-
-    pub fn retrieve_binary_from_vault(&self, vault_key: &str) -> Vec<u8> {
-        self.vault.retrieve(&vault_key.to_string()).unwrap_or_else(|_| Vec::new())
-    }
-
-    pub fn get_field_ptr(&self, _index: usize) -> *const dyn getset::ValueType {
-        panic!("Index out of bounds")
-    }
-
-    pub fn get_field_ptr_mut(&mut self, index: usize) -> *mut dyn getset::ValueType {
-        self.get_field_ptr(index) as *mut _
-    }
-}
-
-impl Drop for AddIn {
-    fn drop(&mut self) {
-        if let Ok(mut client) = self.client.lock() {
-            let _ = client.close(1000, "");
-        }
     }
 }
---
+++ src/addins/ws_client/src/worker.rs
@@ -0,0 +1,139 @@
+use std::sync::mpsc::Sender;
+use std::sync::Arc;
+
+use common_backend::SyncBackendThread;
+use common_binary::vault::BinaryVault;
+use common_logs::Logger;
+use common_tcp::proxy_settings::ProxySettings;
+use common_tcp::tls_settings::TlsSettings;
+
+use crate::client::WebSocketClient;
+
+pub enum WorkerCommand {
+    Connect {
+        url: String,
+        tls: Option<TlsSettings>,
+        proxy: Option<ProxySettings>,
+        headers: Option<Vec<(String, String)>>,
+        response: Sender<String>,
+    },
+    SendText {
+        text: String,
+        response: Sender<String>,
+    },
+    SendBinary {
+        data: Vec<u8>,
+        response: Sender<String>,
+    },
+    ReceiveMessage {
+        timeout_ms: u64,
+        response: Sender<String>,
+    },
+    SendPing {
+        response: Sender<String>,
+    },
+    SendPong {
+        response: Sender<String>,
+    },
+    Close {
+        code: u16,
+        reason: String,
+        response: Sender<String>,
+    },
+    SetLogger {
+        logger: Arc<Logger>,
+        response: Sender<Result<(), String>>,
+    },
+    IsConnected {
+        response: Sender<bool>,
+    },
+    Shutdown,
+}
+
+struct Session {
+    client: WebSocketClient,
+}
+
+impl Session {
+    fn new(vault: BinaryVault) -> Self {
+        Self {
+            client: WebSocketClient::new(vault),
+        }
+    }
+
+    fn is_connected(&self) -> bool {
+        self.client.socket.is_some()
+    }
+}
+
+pub fn spawn_thread(
+    vault: BinaryVault,
+    logger: Option<Arc<Logger>>,
+) -> Result<SyncBackendThread<WorkerCommand>, String> {
+    SyncBackendThread::spawn("opi_wsclient_backend", move |rx| {
+        let mut session = Session::new(vault);
+
+        if let Some(logger) = logger {
+            session.client.set_logger(logger);
+        }
+
+        while let Ok(cmd) = rx.recv() {
+            match cmd {
+                WorkerCommand::Connect {
+                    url,
+                    tls,
+                    proxy,
+                    headers,
+                    response,
+                } => {
+                    if let Some(tls) = tls {
+                        session.client.tls_settings = Some(tls);
+                    }
+                    if let Some(proxy) = proxy {
+                        session.client.proxy_settings = Some(proxy);
+                    }
+                    if let Some(headers) = headers {
+                        session.client.headers = Some(headers);
+                    }
+                    let result = session.client.connect(&url);
+                    let _ = response.send(result);
+                }
+                WorkerCommand::SendText { text, response } => {
+                    let _ = response.send(session.client.send_text(&text));
+                }
+                WorkerCommand::SendBinary { data, response } => {
+                    let _ = response.send(session.client.send_binary(data));
+                }
+                WorkerCommand::ReceiveMessage { timeout_ms, response } => {
+                    let _ = response.send(session.client.receive_message(timeout_ms));
+                }
+                WorkerCommand::SendPing { response } => {
+                    let _ = response.send(session.client.send_ping());
+                }
+                WorkerCommand::SendPong { response } => {
+                    let _ = response.send(session.client.send_pong());
+                }
+                WorkerCommand::Close {
+                    code,
+                    reason,
+                    response,
+                } => {
+                    let _ = response.send(session.client.close(code, &reason));
+                }
+                WorkerCommand::SetLogger { logger, response } => {
+                    session.client.set_logger(logger);
+                    let _ = response.send(Ok(()));
+                }
+                WorkerCommand::IsConnected { response } => {
+                    let _ = response.send(session.is_connected());
+                }
+                WorkerCommand::Shutdown => {
+                    if session.is_connected() {
+                        let _ = session.client.close(1000, "");
+                    }
+                    break;
+                }
+            }
+        }
+    })
+}
